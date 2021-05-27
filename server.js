1fe0ec7134dc931371f13b66191aae560df1004e
KatKava <katryn.kav97@gmail.com>
2021-05-27 16:19:42 +0100

    added back favicon

--- server.js
+++ server.js
@@ -59,6 +59,7 @@ app.use(flash());
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
 
+
 //Server Running
 app.listen(process.env.PORT, () => {
   console.log("Server is running, you better catch it!");
